777a04cb553fc28bcda60d293377d1d5b9e41b3c
leedoekahn401 <ducanh4012006@gmail.com>
2026-03-23 16:12:25 +0700

    Done ex6.*

---
+++ src/main/java/Lab01/ChoosingOption.java
@@ -0,0 +1,27 @@
+package Lab01;
+import javax.swing.JOptionPane;
+
+public class ChoosingOption {
+    public static void main(String[] args){
+        String[] options = {"Yes", "No"};
+        int userChoice = JOptionPane.showOptionDialog(
+                null,
+                "Choose Yes or No",
+                "Choose Yes/No",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        if(userChoice==0){
+            JOptionPane.showMessageDialog(null, "You choose Yes");
+        }else if(userChoice==1){
+            JOptionPane.showMessageDialog(null, "You choose No");
+        }else{
+            JOptionPane.showMessageDialog(null, "U closed the pane");
+        }
+        System.exit(0);
+    }
+}
