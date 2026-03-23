2732c70b65d67d628e6496291761f369c969c2d8
leedoekahn401 <ducanh4012006@gmail.com>
2026-03-23 15:56:47 +0700

    Done ex2.2.5

---
+++ src/main/java/Lab01/ex225.java
@@ -0,0 +1,26 @@
+package Lab01;
+import javax.swing.JOptionPane;
+
+public class ex225 {
+    public static void main(String[] args) {
+        String num1Input = JOptionPane.showInputDialog("Enter first integer");
+        String num2Input = JOptionPane.showInputDialog("Enter first integer");
+        double num1 = Double.parseDouble(num1Input);
+        double num2 = Double.parseDouble(num2Input);
+
+        double sum = num1 + num2;
+        double diff = num1 - num2;
+        double product = num1 * num2;
+        double div = 0;
+        String message = "Sum: "+sum+"\nDiff: "+diff+"\nProduct: "+product+"\n";
+        if(num2 == 0){
+            message = message + "Quotient Invalid";
+            JOptionPane.showMessageDialog(null, message);
+        }else{
+          div = num2 / num1;
+          message = message + "Division: "+div+"\n";
+          JOptionPane.showMessageDialog(null, message);
+        }
+    }
+
+}
