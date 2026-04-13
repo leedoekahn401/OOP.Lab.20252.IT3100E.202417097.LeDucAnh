9b8d24a3eaf3e45f4ef2e84a857ac7f1233fba8d
leedoekahn401 <ducanh4012006@gmail.com>
2026-04-13 13:55:16 +0700

    update

--- src/main/java/AimsProject/src/DigitalVideoDisc.java
+++ src/main/java/AimsProject/src/DigitalVideoDisc.java
@@ -1,50 +1,59 @@
 package AimsProject.src;
 
 public class DigitalVideoDisc {
+
+    private static int nbDigitalVideoDiscs = 0;
+
+    private int id;
+
     private String title;
     private String category;
     private String director;
     private int length;
-    private double cost;
+    private float cost;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getCategory() { return category; }
+    public String getDirector() { return director; }
+    public int getLength() { return length; }
+    public float getCost() { return cost; }
 
 
     public DigitalVideoDisc(String title) {
         this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
-    public DigitalVideoDisc(String title, String category, double cost) {
-        this.title = title;
+    public DigitalVideoDisc(String category, String title, float cost) {
         this.category = category;
+        this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
-    public DigitalVideoDisc(String title, String category, String director, double cost) {
-        this.title = title;
-        this.category = category;
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
         this.director = director;
+        this.category = category;
+        this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
-    public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
-    }
-    public String getTitle(){
-        return title;
-    }
-    public String getCategory(){
-        return category;
-    }
-    public String getDirector(){
-        return director;
-    }
-    public int getLength(){
-        return length;
-    }
-    public double getCost(){
-        return cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 }
