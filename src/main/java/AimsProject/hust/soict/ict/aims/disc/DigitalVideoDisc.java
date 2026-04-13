fd7009733b327f7c2c7496b2ca876268fdb99e97
leedoekahn401 <ducanh4012006@gmail.com>
2026-04-13 16:50:45 +0700

    Update

--- src/main/java/AimsProject/hust/soict/ict/aims/disc/DigitalVideoDisc.java
+++ src/main/java/AimsProject/hust/soict/ict/aims/disc/DigitalVideoDisc.java
@@ -28,17 +28,32 @@ public class DigitalVideoDisc {
         return category;
     }
 
+    public  void setCategory(String category) {
+        this.category = category;
+    }
+
     public String getDirector() {
         return director;
+    }
+
+    public  void setDirector(String director) {
+        this.director = director;
     }
 
     public int getLength() {
         return length;
     }
 
+    public void  setLength(int length) {
+        this.length = length;
+    }
+
     public double getCost() {
         return cost;
     }
+
+
+
 
 
     public DigitalVideoDisc(String title) {
--- src/main/java/AimsProject/hust/soict/ict/aims/test/TestPassingParameter.java
+++ src/main/java/AimsProject/hust/soict/ict/aims/test/TestPassingParameter.java
@@ -12,14 +12,41 @@ public class TestPassingParameter {
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
         System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 
+        swap2(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+
         changeTitle(jungleDVD, cinderellaDVD.getTitle());
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+
+
     }
 
     public static void swap(Object o1, Object o2) {
         Object tmp = o1;
         o1 = o2;
         o2 = tmp;
+    }
+
+    public static void swap2(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        String tmpTitle = dvd1.getTitle();
+        dvd1.setTitle(dvd2.getTitle());
+        dvd2.setTitle(tmpTitle);
+
+        String tmpCategory = dvd1.getCategory();
+        dvd1.setCategory(dvd2.getCategory());
+        dvd2.setCategory(tmpCategory);
+
+        String tmpDirector = dvd1.getDirector();
+        dvd1.setDirector(dvd2.getDirector());
+        dvd2.setDirector(tmpDirector);
+
+        int tmpLength = dvd1.getLength();
+        dvd1.setLength(dvd2.getLength());
+        dvd2.setLength(tmpLength);
+
+
     }
 
     public static void changeTitle(DigitalVideoDisc dvd, String title) {
