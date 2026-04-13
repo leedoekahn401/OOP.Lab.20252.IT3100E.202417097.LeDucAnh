25166ac95f3efc6a9eb8fa89812e9fea8cd1856d
leedoekahn401 <ducanh4012006@gmail.com>
2026-04-13 15:24:14 +0700

    Update

--- src/main/java/AimsProject/hust/soict/ict/aims/test/StoreTest.java
+++ src/main/java/AimsProject/hust/soict/ict/aims/test/StoreTest.java
@@ -7,11 +7,11 @@ public class StoreTest {
     public static void main(String[] args) {
         Store store = new Store();
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99f);
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99);
 
-        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Frozen", "Animation", 20.00f);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Frozen", "Animation", 20.00);
 
         System.out.println("--- Testing Add ---");
         store.addDVD(dvd1);
