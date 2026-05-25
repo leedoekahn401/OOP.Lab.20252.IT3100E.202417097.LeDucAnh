9e4dac038fa982cd7d082479eed712c98981beb0
leedoekahn401 <ducanh4012006@gmail.com>
2026-05-25 15:46:01 +0700

    update painter

--- demo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.finance.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens org.finance.demo to javafx.fxml;
-    exports org.finance.demo;
-}
